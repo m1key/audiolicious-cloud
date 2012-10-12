3460a237749cca521fcf85c0d58d6b12c9db1b76
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-10-12 22:59:26 +0100

    Use velocity for templates.

--- src/main/java/me/m1key/audiolicious/cloud/WelcomeController.java
+++ src/main/java/me/m1key/audiolicious/cloud/WelcomeController.java
@@ -1,5 +1,7 @@
 package me.m1key.audiolicious.cloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WelcomeController {
 
+	private static final Logger log = LoggerFactory
+			.getLogger(WelcomeController.class);
+
 	@RequestMapping(method = RequestMethod.GET, value = "/home")
 	public String handleRequest() {
+		log.info("Welcome called");
 		return "welcome";
 	}
 
