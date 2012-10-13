6a65da06c10399579179e65128d9dfbffa8f4b6c
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-10-13 23:53:08 +0100

    Testing controller's return value.

--- src/test/java/me/m1key/audiolicious/cloud/WelcomeControllerTest.java
+++ src/test/java/me/m1key/audiolicious/cloud/WelcomeControllerTest.java
@@ -1,6 +1,7 @@
 package me.m1key.audiolicious.cloud;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -11,16 +12,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/WEB-INF/audiolicious-servlet.xml"})
+@ContextConfiguration(locations = { "classpath:/WEB-INF/audiolicious-servlet.xml" })
 public class WelcomeControllerTest {
 
 	@Autowired
 	private WelcomeController welcomeController;
 
 	@Test
-	public void returnsCorrectViewName() {
+	public void springShouldInjectTheController() {
 		assertThat(welcomeController, not(nullValue())); // Testing Spring,
 															// really.
+	}
+
+	@Test
+	public void controllerShouldReturnCorrectView() {
+		assertThat(welcomeController.handleRequest(), equalTo("welcome"));
 	}
 
 }
