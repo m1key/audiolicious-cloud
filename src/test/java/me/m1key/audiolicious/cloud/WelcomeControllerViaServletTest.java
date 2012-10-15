2eaa514fb6df2b18f0bb5a7930d6df71118f54b2
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-10-15 21:37:22 +0100

    Testing using spring-test-mvc.
    Sevlet 2.5 (Cloud Foundry requirement).

---
+++ src/test/java/me/m1key/audiolicious/cloud/WelcomeControllerViaServletTest.java
@@ -0,0 +1,34 @@
+package me.m1key.audiolicious.cloud;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/WEB-INF/audiolicious-servlet.xml" })
+public class WelcomeControllerViaServletTest {
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.xmlConfigSetup(
+				"classpath:/WEB-INF/audiolicious-servlet.xml").build();
+	}
+
+	@Test
+	public void testGetBooks() throws Exception {
+		mockMvc.perform(get("/home")).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello!\n")));
+	}
+
+}
