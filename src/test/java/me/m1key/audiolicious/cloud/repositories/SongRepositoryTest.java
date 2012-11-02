7f3a64143674889a49ac26171faeb84f030864bf
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-11-02 22:25:50 +0000

    Added a test for SongRepository.

---
+++ src/test/java/me/m1key/audiolicious/cloud/repositories/SongRepositoryTest.java
@@ -0,0 +1,50 @@
+package me.m1key.audiolicious.cloud.repositories;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import me.m1key.audiolicious.cloud.entities.Song;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/audiolicious-servlet.xml" })
+public class SongRepositoryTest {
+
+	@Autowired
+	private SongRepository songRepository;
+
+	@Before
+	public void setup() {
+		songRepository.deleteAll();
+	}
+
+	@Test
+	public void testRepositoryNotNull() {
+		// Testing Spring & MongoDb, really...
+		assertThat(songRepository, notNullValue());
+	}
+
+	@Test
+	public void testSaveAndRetrieve() {
+		assertThat(songRepository.count(), equalTo(0L));
+
+		Song song = new Song("Come Together");
+		songRepository.save(song);
+
+		assertThat(songRepository.count(), equalTo(1L));
+	}
+
+	@After
+	public void cleanUp() {
+		songRepository.deleteAll();
+	}
+
+}
